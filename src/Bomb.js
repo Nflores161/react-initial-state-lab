90835fab3f7d3f61d93e313c103cb0b0e46063a9
Nick Flores-Tober <nflores161@gmail.com>
2021-04-28 20:52:23 -0400

    commit tree 610a20d9034e6182db92795231462b4addac5520

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,13 @@
-// your Bomb code here!
+import React, { Component } from 'react';
+
+class Bomb extends Component {
+  render() {
+    return(
+      <div>
+      Bomb dot com
+      </div>
+    );
+  }
+}
+
+export default Bomb;
